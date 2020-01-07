5a95cb48f8ac618b2748014653be3f429793f3c5
hickey221 <hickey.221@osu.edu>
2020-01-06 23:17:55 -0500

    foobits setup

--- start.py
+++ start.py
@@ -3,3 +3,9 @@ print("Hello World") #fixed your shit!
 
 # Put that shit in quotes, son
 msg = "im trying to update rich hickey wants a new motorcycle to replace scarlett"
+
+# Can you see this?
+
+# it's a goddamn miracle!'
+
+# This is some new shit
