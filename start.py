4b79fd1581d74015a1730b679f652aa6e766cbab
hickey221 <hickey.221@osu.edu>
2020-01-07 00:17:14 -0500

    commit of collaboration session

--- start.py
+++ start.py
@@ -1,11 +1,42 @@
 #get rekt richard
-print("Hello World") #fixed your shit!
+#print("Hello World") #fixed your shit!
+
+import time
+starttime = time.process_time()
+
+#for n in range(10):
+    # stuff happens here
+   # print("Right now, the number is", n)
+
+my_favorite_words = ["baby yoda", "horse", "fuck", "long", "lizard", "list", "of", "words", "coding", "zebra"]
+words_i_know = []
+for i in my_favorite_words:
+    print("computing", i)
+    if i in words_i_know:
+        # This will ONLY happen if a repeat is found
+        print("you already know", i)
+        break
+    else:
+        # Using this 'else' is optional but okay!
+        words_i_know.append(i)
+    if "z" in i:
+        print("we dont need no z's around here")
+        words_i_know.pop()
+        print("deleting", i)
+print(words_i_know)
+words_i_know.
+#print(endtime - starttime)
 
 # Put that shit in quotes, son
 msg = "im trying to update rich hickey wants a new motorcycle to replace scarlett"
 
 # Can you see this?
 
-# it's a goddamn miracle!'
-
 # This is some new shit
+
+
+# Learning new words
+# But I have a limited memory!
+# Teach me 10 words from a long list of words
+# (Don't teach me repeats, but don't break for them either)
+# But if I learn more than 10, I forget the oldest word I know!
