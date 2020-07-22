986674ac1d06312a1879e90580191b22eb36b35c
hickey221 <hickey.221@osu.edu>
2020-07-22 18:51:48 -0400

    init backpack problem

---
+++ backpack.py
@@ -0,0 +1,15 @@
+"""
+Given a set of items (from a text file) that have a given weight and value, find
+the combination of items of greatest value that fit into a bag of limited space.
+
+"""
+# Initialize variables
+max_weight = 25
+item_file = 'items.txt'
+multiple_uses = False
+
+# 1. Read in items from file
+
+
+# 2. Solve the backpack problem :)
+
